ce4ee49fb81e89bcf5ba59b01aa5fba362c03a20
Nitish Puri <npuri1903@gmail.com>
2019-09-07 18:03:05 +0530

    add obj reader..

---
+++ 2_mesh.py
@@ -0,0 +1,4 @@
+from objreader import *
+
+
+m = Model("obj/african_head.obj")
---
+++ objreader.py
@@ -0,0 +1,23 @@
+
+
+class Model:
+    def __init__(self, filename):
+        self.filename = filename
+        self.vertices = []
+        self.faces = []
+        self.__read()
+
+    def __read(self):
+        with open(self.filename, 'r') as file:
+            for line in file:
+                data = line.split()
+                if line.startswith('v '):   # vertex
+                    v = [float(x) for x in data[1:]]
+                    self.vertices.append(v)
+                if line.startswith('f '):   # face
+                    f = [x.split('/') for x in data[1:]]
+                    self.faces.append(f)
+
+        print("Read :: ", self.filename)
+        print("# vertices = ", len(self.vertices))
+        print("# faces = ", len(self.faces))
