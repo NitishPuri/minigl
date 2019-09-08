efd441eca624f1cc7557561c193041bd443ba470
Nitish Puri <npuri1903@gmail.com>
2019-09-09 00:11:20 +0530

    add performance timer.

--- 2_mesh_wireframe.py
+++ 2_mesh_wireframe.py
@@ -2,6 +2,7 @@ from objreader import *
 from minigl import *
 import utils
 from gui import MainWindow
+import time
 
 width = 800
 height = 800
@@ -13,16 +14,27 @@ def map_to_screen(v):
 
 
 if __name__ == "__main__":
+
+    timestart = time.perf_counter()
     image = utils.createImage(width, height)
     m = Model("obj/african_head.obj")
+    timeend = time.perf_counter()
+    print("Read model :: ", (timeend - timestart), "ms")
+
+    timestart = time.perf_counter()
 
     for i, f in enumerate(m.faces):
         v = [map_to_screen(m.vertices[f[i][0]]) for i in range(3)]
         line(v[0], v[1], image, utils.WHITE)
         line(v[1], v[2], image, utils.WHITE)
         line(v[0], v[2], image, utils.WHITE)
+    timeend = time.perf_counter()
+    print("Render image :: ", (timeend - timestart), "ms")
 
+    timestart = time.perf_counter()
     utils.saveImage("out/2_mesh_wireframe.jpg", image)
+    timeend = time.perf_counter()
+    print("Save to file :: ", (timeend - timestart), "ms")
 
     window = MainWindow(width, height)
     window.showImage(image)
