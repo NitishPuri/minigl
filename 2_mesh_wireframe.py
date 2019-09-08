0f73ad19c9eeec7ae4a3f620053e5564487f5b2b
Nitish Puri <npuri1903@gmail.com>
2019-09-08 17:05:17 +0530

    wireframe mesh example using main window

--- 2_mesh_wireframe.py
+++ 2_mesh_wireframe.py
@@ -1,23 +1,31 @@
 from objreader import *
 from minigl import *
 import utils
+from gui import MainWindow
+
+import tkinter
+from PIL import Image, ImageTk
 
 width = 800
 height = 800
 padding = 10
-
-image = utils.createImage(width, height)
-m = Model("obj/african_head.obj")
 
 
 def map_to_screen(v):
     return [(v[0] + 1) * (width - padding) / 2, (v[1] + 1) * (height - padding)/2, v[2]]
 
 
-for i, f in enumerate(m.faces):
-    v = [map_to_screen(m.vertices[f[i][0]]) for i in range(3)]
-    line(v[0], v[1], image, utils.WHITE)
-    line(v[1], v[2], image, utils.WHITE)
-    line(v[0], v[2], image, utils.WHITE)
+if __name__ == "__main__":
+    image = utils.createImage(width, height)
+    m = Model("obj/african_head.obj")
 
-utils.saveImage("out/2_mesh_wireframe.jpg", image)
+    for i, f in enumerate(m.faces):
+        v = [map_to_screen(m.vertices[f[i][0]]) for i in range(3)]
+        line(v[0], v[1], image, utils.WHITE)
+        line(v[1], v[2], image, utils.WHITE)
+        line(v[0], v[2], image, utils.WHITE)
+
+    utils.saveImage("out/2_mesh_wireframe.jpg", image)
+
+    window = MainWindow(width, height)
+    window.showImage(image)
