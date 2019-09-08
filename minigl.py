9a39e0921ad2faf6755d69624f8d2cca1a57ff25
Nitish Puri <npuri1903@gmail.com>
2019-09-08 17:04:50 +0530

    move incomplete triangle code to minigl

--- minigl.py
+++ minigl.py
@@ -13,9 +13,6 @@ def aligned_rectangle(p, q, image, color):
     image[p[0]:q[0], p[1]:q[1]] = color
 
 
-# Fourth attempt
-
-
 def line(a, b, image, color):
     a = np.asarray(a[:2])
     b = np.asarray(b[:2])
@@ -37,3 +34,32 @@ def line(a, b, image, color):
         if error > 0.5:
             y += 1 if b[1] > a[1] else -1
             error -= 1
+
+
+def triangle(a, b, c, image, color):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+    if a[1] > b[1]:
+        a, b = b, a
+    if a[1] > c[1]:
+        a, c = c, a
+    if b[1] > c[1]:
+        a, c = c, a
+    total_height = c[1] - a[1]
+    seg_height = b[1] - a[1]
+    for y in range(a[1], b[1]+1):
+        alpha = (y - a[1]) / total_height
+        beta = (y - a[1]) / seg_height
+        A = a + (c - a) * alpha
+        B = a + (b - a) * beta
+        pixel(A, image, color)
+        pixel(B, image, color)
+    # seg_height = c[1] - b[1]
+    # for y in range(b[1], c[1]):
+    #     alpha = (y - a[1]) / total_height
+    #     beta = (y - b[1]) / seg_height
+    #     A = a + (c - a) * alpha
+    #     B = a + (c - b) * beta
+    #     pixel(A, image, utils.RED)
+    #     pixel(B, image, utils.GREEN)
